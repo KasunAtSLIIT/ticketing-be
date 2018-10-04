ffa5f9cd9d28cf23f3867ea70bf0b9ae81deeeaf
Charuni Rajapakshe <charunipoornima@gmail.com>
2018-10-04 19:46:10 +0530

    end

--- src/main/java/com/csse/ticketing/serviceImpl/JourneyServiceImpl.java
+++ src/main/java/com/csse/ticketing/serviceImpl/JourneyServiceImpl.java
@@ -1,7 +1,52 @@
 package com.csse.ticketing.serviceImpl;
 
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.csse.ticketing.models.JourneyModel;
+import com.csse.ticketing.models.UserModel;
+import com.csse.ticketing.repository.JourneyRepository;
 import com.csse.ticketing.services.JourneyService;
 
+/**
+ * 
+ * @author Charuni
+ *
+ */
+
+@Service
 public class JourneyServiceImpl implements JourneyService{
+	
+	@Autowired 
+	private JourneyRepository journeyRepo;
+
+	@Override
+	public JourneyModel createJourney(JourneyModel journey) {
+		// TODO Auto-generated method stub
+		journey.setKey(UUID.randomUUID().toString());
+		return journeyRepo.save(journey);
+	}
+
+	@Override
+	public JourneyModel updateJourney(JourneyModel journey) {
+		// TODO Auto-generated method stub
+		return journeyRepo.save(journey);
+	}
+
+	@Override
+	public JourneyModel retriveJourney(String id) {
+		// TODO Auto-generated method stub
+		return journeyRepo.findOne(id);
+	}
+
+	@Override
+	public List<JourneyModel> listJourney(String user) {
+		// TODO Auto-generated method stub
+		return journeyRepo.findAll();
+	}
+	
 
 }
--- src/main/java/com/csse/ticketing/services/JourneyService.java
+++ src/main/java/com/csse/ticketing/services/JourneyService.java
@@ -1,5 +1,39 @@
 package com.csse.ticketing.services;
 
+import java.util.List;
+
+import com.csse.ticketing.models.JourneyModel;
+
 public interface JourneyService {
+	
+	/**
+	 * @author Charuni
+	 * @param journey
+	 * @return
+	 */
+	public JourneyModel createJourney(JourneyModel journey);
+	
+	/**
+	 * @author Charuni
+	 * @param journey
+	 * @return
+	 */
+	public JourneyModel updateJourney(JourneyModel journey);
+	
+	/**
+	 * @author Charuni
+	 * @param id
+	 * @return
+	 */
+	public JourneyModel retriveJourney(final String id);
+	
+	/**
+	 * @author Charuni
+	 * @param user
+	 * @return
+	 */
+	public List<JourneyModel> listJourney(final String user);
+
+	
 
 }
